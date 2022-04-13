613ea104471d85f23688c0b6849719914e486184
vincent8914 <c44051@gmail.com>
2022-04-13 14:37:18 +0800

    update simple input method of BST

--- src/binarySearchTree/Main.java
+++ src/binarySearchTree/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		BinarySearchTree bts = new BinarySearchTree();
 		Scanner input = new Scanner(System.in);
+		
 		try {
 			int i = input.nextInt();
 
