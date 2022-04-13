ced2b6d333ffb9abcc091fda61c6d172a6d00df3
vincent8914 <c44051@gmail.com>
2022-04-13 14:04:06 +0800

    4/13

--- dataStructure/src/binarySearchTree/Main.java
+++ dataStructure/src/binarySearchTree/Main.java
@@ -1,28 +1,43 @@
 package binarySearchTree;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		BinarySearchTree bts = new BinarySearchTree();
-		
-		bts.insert(new Node(3));
-		bts.insert(new Node(1));
-		bts.insert(new Node(5));
-		bts.insert(new Node(4));
-//		bts.insert(new Node(2));
-		bts.insert(new Node(9));
-		bts.insert(new Node(8));
-		bts.insert(new Node(10));
-		bts.insert(new Node(6));
-		bts.insert(new Node(0));
-		bts.insert(new Node(-2));
-		bts.insert(new Node(-1));
-//		bts.insert(new Node(11));
-		
+		Scanner input = new Scanner(System.in);
+		try {
+			int i = input.nextInt();
+
+			for (; i != 7414;) {
+				bts.insert(new Node(i));
+				i = input.nextInt();
+			}
+		} catch (Exception e) {
+			System.out.println("err");
+		} finally {
+			input.close();
+		}
+
+//		bts.insert(new Node(3));
+//		bts.insert(new Node(1));
+//		bts.insert(new Node(5));
+//		bts.insert(new Node(4));
+////		bts.insert(new Node(2));
+//		bts.insert(new Node(9));
+//		bts.insert(new Node(8));
+//		bts.insert(new Node(10));
+//		bts.insert(new Node(6));
+//		bts.insert(new Node(0));
+//		bts.insert(new Node(-2));
+//		bts.insert(new Node(-1));
+////		bts.insert(new Node(11));
+
 		bts.display();
-		
+
 		System.out.println(bts.search(6));
-		
+
 		bts.delete(1);
 		bts.display();
 	}
